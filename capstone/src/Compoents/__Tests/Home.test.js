e59712d47d1d068a430c2b23f5994ae3a84926e4
vikasini55 <79197319+vikasini55@users.noreply.github.com>
2023-03-28 11:15:10 +0530

    Add files via upload

---
+++ capstone/src/Compoents/__Tests/Home.test.js
@@ -0,0 +1,43 @@
+import { screen, render } from "@testing-library/react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "../Home";
+
+describe("Home Component", () => {
+  it("checks whether Image is Present", () => {
+    render(
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    );
+    expect(screen.getByAltText("First slide")).toBeInTheDocument();
+  });
+  it("checks whether Image is displayed", () => {
+    render(
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    );
+    expect(screen.getByAltText("Second slide")).toBeInTheDocument();
+  });
+
+  it("checks whether Image is present", () => {
+    render(
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    );
+    expect(screen.getByRole("img",{name:/Thrid slide/i})).toBeInTheDocument();
+  });
+
+
+});
+
+
+
+
